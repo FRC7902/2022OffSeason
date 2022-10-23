0d6e98fd83d4d4f0e8aa712bcc50f59b49c8235b
Jane <janezeng45@gmail.com>
2022-10-23 10:29:18 -0400

    update deprecated speedcontroller to motorcontroller

--- 2022OffSeason/src/main/java/frc/robot/Robot.java
+++ 2022OffSeason/src/main/java/frc/robot/Robot.java
@@ -13,7 +13,7 @@ import edu.wpi.first.wpilibj.Joystick;
 
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.RobotController;
-import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX;
@@ -46,8 +46,8 @@ public class Robot extends TimedRobot {
   private PWMVictorSPX right1 = new PWMVictorSPX(2);
   private PWMVictorSPX right2 = new PWMVictorSPX(3);
 
-  private SpeedControllerGroup left = new SpeedControllerGroup(left1, left2);
-  private SpeedControllerGroup right = new SpeedControllerGroup(right1, right2);
+  private MotorControllerGroup left = new MotorControllerGroup(left1, left2);
+  private MotorControllerGroup right = new MotorControllerGroup(right1, right2);
 
   private DifferentialDrive drive = new DifferentialDrive(left, right);
 
@@ -81,8 +81,6 @@ public class Robot extends TimedRobot {
     new Pose2d(4, 5, new Rotation2d())
   );
  
-
-  
 
   /**
    * This function is run when the robot is first started up and should be used
