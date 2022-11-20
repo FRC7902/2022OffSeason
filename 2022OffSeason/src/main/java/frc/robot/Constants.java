ee4e5f0019b264abe0ce8db44bab3a8d2284aee6
Arjun Bhatia <36422829+ArgonBoron23@users.noreply.github.com>
2022-11-20 10:13:03 -0500

    Moved drive code into separate subsystem

--- 2022OffSeason/src/main/java/frc/robot/Constants.java
+++ 2022OffSeason/src/main/java/frc/robot/Constants.java
@@ -5,11 +5,40 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+
+    public final static class DriveConstants {
+
+        // PWM ports for drive motors
+        public static final int kLeftLeader = 0;
+        public static final int kLeftFollower = 1;
+        public static final int kRightLeader = 2;
+        public static final int kRightFollower = 3;
+
+
+        public static final int kLeftEncoderA = 0;
+        public static final int kLeftEncoderB = 1; 
+        public static final int kLeftEncoderIndex = 2;
+
+        public static final int kRightEncoderA = 3; 
+        public static final int kRightEncoderB = 4; 
+        public static final int kRightEncoderIndex = 5;
+
+        public static final int gyroChannel = 1;
+
+
+
+    }
+
+}
