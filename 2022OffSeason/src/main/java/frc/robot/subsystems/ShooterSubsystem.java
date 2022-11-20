4efb64346f9c88a8925b32b51902b19e84ad2f1d
darren4141 <109255883+darren4141@users.noreply.github.com>
2022-11-20 11:06:30 -0500

    added shootersubsystem

--- 2022OffSeason/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ 2022OffSeason/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,20 +1,52 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+import com.ctre.phoenix.motorcontrol.InvertType;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import frc.robot.Constants;
 
 public class ShooterSubsystem extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
 
-  public ShooterSubsystem() {}
+  //Delcaring new motors
+  private final WPI_VictorSPX leaderMotor = new WPI_VictorSPX(Constants.ShooterConstants.ShooterLeaderCAN);
+  private final WPI_VictorSPX followerMotor = new WPI_VictorSPX(Constants.ShooterConstants.ShooterFollowerCAN);
 
-  private final VictorSPX leaderMotor = new VictorSPX(Constants.ShooterConstants.ShooterLeaderCAN);
-  private final VictorSPX followerMotor = new VictorSPX(Constants.ShooterConstants.ShooterFollowerCAN);
+  public ShooterSubsystem() {
+
+    //follower motor will follow everything leader does
+    followerMotor.follow(leaderMotor);
+
+    //follower follows leader even in inversion
+    leaderMotor.setInverted(false);
+    followerMotor.setInverted(InvertType.FollowMaster);
+
+  }
+
+
+  //Sets output power
+  //double power in range [-1, 1]
+  public void setPower(double power){
+
+    //set output power of motor to input speed
+    leaderMotor.set(power);
+
+  }
+
+  //stops the motor
+  public void stopMotors(){
+
+    //stops motor
+    leaderMotor.stopMotor();
+
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    // For example, printing values to smart dashboard or shuffleboard
+
   }
 
   @Override
